e620890e3e7aebdc5b8546f28926e58e6242818c
Aashish Chapain <ashishchapain86@gmail.com>
2024-04-23 02:48:00 +0000

    updated content

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/purple_background.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "purple abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a Fullstack Developer and the students of Computer Science. I love to solve the real world problems with the help of software. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,10 +32,9 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Backen Development",
+  "Frontend Development",
+  "Database Design",
   "Graphic design",
 ];
 
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I build softwares that helps to make the world better place to live";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/book_table.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desktop with typewriter and books";
 
 /**
  * Project list
@@ -31,28 +31,17 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Sociobytes",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Sociobytes is a link-sharing platform. It was built with Django in the backend and Bootstrap in the frontend.",
+    url: "https://sociobytes.com",
   },
+
   {
-    title: "Web Development for Beginners",
+    title: "Geekydocs",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Geekydocs is a blogging website. It was built using Django as a backend and markdown library as a markdown renderer",
+    url: "https://geekydocs.com",
   },
 ];
 
